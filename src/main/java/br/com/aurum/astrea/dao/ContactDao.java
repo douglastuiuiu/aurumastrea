7842516920ee6e7559747cb14c900c538b894aa6
Douglas Gomes <douglas.gomes@calcard.com.br>
2017-06-04 12:10:41 -0300

    DAO ok

--- src/main/java/br/com/aurum/astrea/dao/ContactDao.java
+++ src/main/java/br/com/aurum/astrea/dao/ContactDao.java
@@ -1,28 +1,30 @@
 package br.com.aurum.astrea.dao;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import br.com.aurum.astrea.domain.Contact;
 import com.googlecode.objectify.ObjectifyService;
 
-import br.com.aurum.astrea.domain.Contact;
+import java.util.List;
+
+import static com.googlecode.objectify.ObjectifyService.ofy;
 
 public class ContactDao {
-	
-	static {
-		ObjectifyService.register(Contact.class);
-	}
-	
-	public void save(Contact contact) {
-		// TODO: É preciso pesquisar como se usa o Objectify para armazenar a entidade contato no banco de dados.
-	}
-	
-	public List<Contact> list() {
-		// TODO: É preciso pesquisar como se usa o Objectify para listar as entidades de contato.
-		return new ArrayList<>();
-	}
-	
-	public void delete(Long contactId) {
-		// TODO: É preciso pesquisar como se usa o Objectify para deletar entidades do banco de dados.
-	}
+
+    static {
+        ObjectifyService.register(Contact.class);
+    }
+
+    public void save(Contact contact) {
+        ofy().save().entity(contact).now();
+    }
+
+    public List<Contact> list() {
+        return ofy().load().type(Contact.class).list();
+    }
+
+    public void delete(Long id) {
+        Contact contact = ObjectifyService.ofy().load().type(Contact.class).id(id).now();
+        if (contact != null) {
+            ofy().delete().entity(contact).now();
+        }
+    }
 }
