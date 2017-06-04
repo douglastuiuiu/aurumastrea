4e956edf37128b0183d4ea0fd18805297eb6585e
Douglas Gomes <douglas.gomes@calcard.com.br>
2017-06-04 12:13:38 -0300

    servlet OK

--- src/main/java/br/com/aurum/astrea/service/ContactServlet.java
+++ src/main/java/br/com/aurum/astrea/service/ContactServlet.java
@@ -1,34 +1,38 @@
 package br.com.aurum.astrea.service;
 
-import java.io.IOException;
+import br.com.aurum.astrea.dao.ContactDao;
+import br.com.aurum.astrea.domain.Contact;
+import com.google.appengine.repackaged.com.google.gson.Gson;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import br.com.aurum.astrea.dao.ContactDao;
+import java.io.IOException;
+import java.util.List;
 
 @SuppressWarnings("serial")
 public class ContactServlet extends HttpServlet {
-	
-	private static final ContactDao DAO = new ContactDao();
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
-		// TODO: Implementar um método que irá ler o corpo da requisição e, com essas informações,
-		// salvar no banco de dados uma entidade do tipo 'Contato' com essas informações.
-	}
-	
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
-		// TODO: Implementar um método que irá listar todas as entidades do tipo 'Contato' e devolver para o client essa listagem.
-	}
-	
-	@Override
-	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-		
-		// TODO: Implementar um método que irá deletar uma entidade do tipo 'Contato', dado parâmetro de identificação.
-	}
+    private static final ContactDao DAO = new ContactDao();
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Contact contact = new Gson().fromJson(req.getReader(), Contact.class);
+        DAO.save(contact);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        List<Contact> contacts = DAO.list();
+        String json = new Gson().toJson(contacts);
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write(json);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Long id = Long.parseLong(req.getParameter("id"));
+        DAO.delete(id);
+    }
 }
