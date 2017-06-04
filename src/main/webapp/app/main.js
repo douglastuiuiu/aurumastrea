8017462abe64eafb8543a24212c6f80d9f4237f7
Douglas Gomes <douglas.gomes@calcard.com.br>
2017-06-04 10:31:06 -0300

    code format

--- src/main/webapp/app/main.js
+++ src/main/webapp/app/main.js
@@ -1,30 +1,33 @@
-var app = angular.module('avaliacandidatos', [ 'ngRoute', 'ui.router', 'ui.bootstrap']);
+var app = angular.module('avaliacandidatos', ['ngRoute', 'ui.router', 'ui.bootstrap']);
 
-app.config([ '$urlRouterProvider', '$stateProvider',
-		function($urlRouterProvider, $stateProvider) {
-			
-			$stateProvider.state('main', {url:'/', templateUrl:'/view/main.html'});
-			$stateProvider.state('main.contacts', {url:'contacts', templateUrl:'/view/contacts/contacts.html'});
-			$stateProvider.state('main.addeditcontact', {url:'addeditcontact', templateUrl:'/view/contacts/contactaddedit.html'});
-			
-			$urlRouterProvider.otherwise("/")
-			
-		} ]);
+app.config(['$urlRouterProvider', '$stateProvider',
+    function ($urlRouterProvider, $stateProvider) {
 
-app.directive('ngBack', function() {
-	return function(scope, element, attrs) {
-		element.bind('click', function(evt) {
-			history.back();
-		});
-	};
+        $stateProvider.state('main', {url: '/', templateUrl: '/view/main.html'});
+        $stateProvider.state('main.contacts', {url: 'contacts', templateUrl: '/view/contacts/contacts.html'});
+        $stateProvider.state('main.addeditcontact', {
+            url: 'addeditcontact',
+            templateUrl: '/view/contacts/contactaddedit.html'
+        });
+
+        $urlRouterProvider.otherwise("/")
+
+    }]);
+
+app.directive('ngBack', function () {
+    return function (scope, element, attrs) {
+        element.bind('click', function (evt) {
+            history.back();
+        });
+    };
 });
 
-mainController = function($scope, $window) {
-    return $window.initRoot = function() {
-      $scope.backend_ready = true;
-      return $scope.$apply();
+mainController = function ($scope, $window) {
+    return $window.initRoot = function () {
+        $scope.backend_ready = true;
+        return $scope.$apply();
     };
-  };
+};
 
 app.controller('mainController', mainController);
-app.$inject = [ '$scope', '$http', '$cookies' ];
+app.$inject = ['$scope', '$http', '$cookies'];
